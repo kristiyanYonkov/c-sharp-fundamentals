48c4d2c2578b32a9da8ac9f261f9cff5afd1f776
kristiyanYonkov <todorovk03@gmail.com>
2024-01-15 11:15:57 +0200

    Refactor Main method

--- HelloGitHub/HelloGitHub/Program.cs
+++ HelloGitHub/HelloGitHub/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, GitHub!");
+
+            Employee employeeOne = new Employee("Georgi", "Parvano", 58, "President", "Sofia", "Public Affairs");
+            employeeOne.DisplayEmployeeInfo();
         }
+
     }
 }
