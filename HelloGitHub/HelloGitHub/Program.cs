5e1c8906d97a5cc155cce2c18eea2eecef40b836
kristiyanYonkov <todorovk03@gmail.com>
2024-01-15 13:42:22 +0200

    Add Vehicle class to Program.cs

--- HelloGitHub/HelloGitHub/Program.cs
+++ HelloGitHub/HelloGitHub/Program.cs
@@ -8,6 +8,16 @@
 
             Employee employeeOne = new Employee("Georgi", "Parvano", 58, "President", "Sofia", "Public Affairs");
             employeeOne.DisplayEmployeeInfo();
+
+            Console.WriteLine();
+
+            Company companyOne = new Company("BitSerious", 1888, "Belgrade", true);
+            companyOne.DisplayCompanyInfo();
+
+            Console.WriteLine();
+
+            Vehicle vehicleOne = new Vehicle("Volvo XC90", "Petrol", "automatic", 2024);
+            vehicleOne.DisplayVehicleInfo();
         }
 
     }
