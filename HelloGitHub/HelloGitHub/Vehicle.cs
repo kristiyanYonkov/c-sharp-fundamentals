b6f222e44db231e446bdf23ef9c3596a0b4dd406
kristiyanYonkov <todorovk03@gmail.com>
2024-01-15 13:41:27 +0200

    Initialise Vehicle class

---
+++ HelloGitHub/HelloGitHub/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGitHub
+{
+    internal class Vehicle
+    {
+        public string model;
+        public string engine;
+        public string transmission;
+        public int year;
+
+        public Vehicle(string vehicleModel, string vehicleEngine, string vehicleTransmission, int vehicleYear)
+        {
+            model = vehicleModel; 
+            engine = vehicleEngine; 
+            transmission = vehicleTransmission;
+            year = vehicleYear;
+        }
+
+        public void DisplayVehicleInfo()
+        {
+            Console.Write($"{model}\n{engine}\n{transmission}\n{year}");
+        }
+    }
+}
