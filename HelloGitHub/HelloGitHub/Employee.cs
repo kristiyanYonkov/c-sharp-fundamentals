4b68cff60a86e4780aae2b6c9844d2418f9170eb
kristiyanYonkov <todorovk03@gmail.com>
2024-01-15 11:06:59 +0200

    Add new functionality

---
+++ HelloGitHub/HelloGitHub/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGitHub
+{
+    internal class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public string position;
+        public string city;
+        public string department;
+
+        public Employee(string employeeName, 
+            string employeeLastName,
+            int employeeAge,
+            string employeePosition,
+            string employeeCity,
+            string employeeDepartment)
+        {
+            firstName = employeeName;
+            lastName = employeeLastName;    
+            age = employeeAge;
+            position = employeePosition;
+            city = employeeCity;
+            department = employeeDepartment;
+        }
+
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"Hello! I am {firstName} {lastName}, from {city}.\nI am {age} years old and working as {position} in {department}");
+        }
+
+    }
+}
